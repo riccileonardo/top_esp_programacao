44f2e17c7aba9a129803591edd6466432ebd0f8a
riccileonardo <l.lopesricci@gmail.com>
2025-08-26 22:11:00 -0400

    atividades aula 2

---
+++ arquivo1.py
@@ -0,0 +1,11 @@
+from openai import OpenAI
+
+client = OpenAI()
+
+response = client.responses.create(
+  model="gpt-4o-mini",
+  input="write a haiku about ai",
+  store=True,
+)
+
+print(response.output_text);
